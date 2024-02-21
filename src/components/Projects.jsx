21e03813f301afebeaa93d95e013ca5264047c9c
Manish <manishkr9900@gmail.com>
2024-02-21 23:54:41 +0530

    added go back button in all projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
 function Projects() {
+  const navigate = useNavigate();
   return (
-    <div><Navbar/></div>
-  )
+    <div>
+      <Navbar />
+      <div className="h-[100vh] w-full flex flex-col gap-5 justify-center items-center">
+        <div className="text-white">Work in Progress..................</div>
+        <div>
+          <button
+            className="border-2 border-white bg-slate-50 shadow-xl"
+            onClick={() => navigate(-1)}
+          >
+            Go back
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
