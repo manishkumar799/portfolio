94a42eb5bea6413e9db50f0f6ad14e69231bde27
Manish <manishkr9900@gmail.com>
2024-05-19 15:05:10 +0530

    page refresh issue fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:"/portfolio",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
